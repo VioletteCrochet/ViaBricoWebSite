633b6386bb3c2e59d6c267972269b6ecc5693257
VADILLO Matthieu <mat.vadillo@orange.fr>
2019-04-30 09:07:02 +0200

    recup modal

---
+++ js/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/ajouter-un-fournisseur', (req, res) => {
+    res.sendFile(dirname + '/ajouter.html')
+});
+
+app.get('/connexion', (req, res) => {
+    res.sendFile(dirname + '/connexion.html')
+});
+
+app.get('/fiche-détaillée', (req, res) => {
+    res.sendFile(dirname + '/fichedetaillee.html')
+});
+
+app.get('/liste-des-fournisseurs', (req, res) => {
+    res.sendFile(dirname + '/fichedetaillee.html')
+});
+
+module.exports = { app };
