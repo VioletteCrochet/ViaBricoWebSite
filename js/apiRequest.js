c2b6f1d2100d29b34ae56549214ab29454be8ad7
VioletteCrochet <violettecrc@gmail.com>
2019-05-03 09:02:26 +0200

    rerouting

--- js/apiRequest.js
+++ js/apiRequest.js
@@ -15,6 +15,23 @@ const postUser = () => {
     });
 };
 
+const postFournisseur = () => {
+    return new Promise((resolve, reject) => {
+        request( {
+            url: 'https://viabricosrm.herokuapp.com/api/fournisseur',
+            json: true
+        }, (error, response, body) => {
+            if(!body) {
+                reject(error);
+            } else {
+                resolve(body);
+            }
+        });
+    });
+};
+
+
 module.exports = {
-    postUser
+    postUser,
+    postFournisseur
 };
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/ajouter-un-fournisseur', (req, res) => {
+app.get('/ajouter', (req, res) => {
     res.sendFile(__dirname + '/ajouter.html')
 });
 
@@ -10,13 +10,27 @@ app.get('/connexion', (req, res) => {
     res.sendFile(__dirname + '/connexion.html')
 });
 
-app.get('/fiche-détaillée', (req, res) => {
+app.get('/fiche-detaille', (req, res) => {
     res.sendFile(__dirname + '/fichedetaillee.html')
 });
 
-app.get('/liste-des-fournisseurs', (req, res) => {
+app.get('/fournisseurs', (req, res) => {
     res.sendFile(__dirname + '/fournisseurs.html')
 });
+
+app.get('/fournisseurs', (req, res) => {
+    res.sendFile(__dirname + '/fournisseurs.html')
+});
+
+app.post('/fournisseurs', (req, res) => {
+    res.sendFile(__dirname + '/fournisseurs.html')
+});
+
+app.post('/connexion', (req, res) => {
+    
+});
+
+
 
 // app.post('/connexion', (req, res) => {
 //     res.
